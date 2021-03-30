7a8e0dbf38f23787787798acced4b93e478a42f1
katarro <66927047+katarro@users.noreply.github.com>
2021-03-30 06:58:41 -0600

    Cambio en el inicio Se le agregó las instrucciones para utilizar el servicio web, mediante la insstalación del plugin CORS unblocked

--- src/App.js
+++ src/App.js
@@ -1,62 +1,46 @@
-import React from 'react';
-import { useState, useEffect } from 'react'; // Hooks
-import './App.css';
-import { getPokemonData } from './components/Api';
-import Navbar from './components/Navbar'
-import Routes from './components/Routes';
+import React from "react";
+import { useState, useEffect } from "react"; // Hooks
+import "./App.css";
+import { getPokemonData } from "./components/Api";
+import Navbar from "./components/Navbar";
+import Routes from "./components/Routes";
 
 function App(props) {
-	const [pokemons, setPokemons] = useState([]);
-	const min = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=119';
-	//const max = 'https://pokeapi.co/api/v2/pokemon?offset=166&limit=818';
+  const [pokemons, setPokemons] = useState([]);
+  const min = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=700";
+  //const max = 'https://pokeapi.co/api/v2/pokemon?offset=166&limit=818';
 
-	const fetchPokemons = async () => {
-		const getPokemons = async () => {
-			try {
-				const response = await fetch(min);
-				const data = await response.json();
-				return data;
-			} catch (error) {}
-		};
+  const fetchPokemons = async () => {
+    const getPokemons = async () => {
+      try {
+        const response = await fetch(min);
+        const data = await response.json();
+        return data;
+      } catch (error) {}
+    };
 
-		/* const getPokemons2 = async () => {
-			try {
-				const response = await fetch(max);
-				const data = await response.json();
-				return data;
-			} catch (error) {}
-		}; */
+    const data = await getPokemons();
+    const promises = data.results.map(async (pokemon) => {
+      return await getPokemonData(pokemon.url);
+    });
 
-		/********************************************** */
+    const results = await Promise.all(promises);
+    //Fusionar los arreglos
 
-		const data = await getPokemons();
-		const promises = data.results.map(async (pokemon) => {
-			return await getPokemonData(pokemon.url);
-		});
+    //const resultados = results.concat(results2);
+    setPokemons(results);
+  };
 
-		/* const data2 = await getPokemons2();
-		const promises2 = data2.results.map(async (pokemon) => {
-			return await getPokemonData(pokemon.url);
-		}); */
-		//const results2 = await Promise.all(promises2);
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
 
-		const results = await Promise.all(promises);
-		//Fusionar los arreglos
-
-		//const resultados = results.concat(results2);
-		setPokemons(results);
-	};
-
-	useEffect(() => {
-		fetchPokemons();
-	}, []);
-
-	return (
-		<React.Fragment>
-			<Navbar/>
-			<Routes pokemons={pokemons} />
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Routes pokemons={pokemons} />
+    </React.Fragment>
+  );
 }
 
 export default App;
--- src/components/Start.js
+++ src/components/Start.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Navbar from './Navbar';
-import '../Styles/Start.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
+import "../Styles/Start.css";
 export default function Start() {
-	var img = 'https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg';
+  var img =
+    "https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg";
 
-	return (
-		<React.Fragment>
+  return (
+    <React.Fragment>
+      <div className="container" id="container-Start">
+        <div className="row3">
+          <div className="col-12">
+            <img src={img} alt="Pokemons" />
+          </div>
 
-			<div className="container" id="container-Start">
-				<div className="row3">
-					<div className="col-12">
-						<img src={img} alt="Pokemons" />
-					</div>
-
-					<div className="col-12" id="button-Start">
-						<Link to="/pokemons">
-							<button className="btn btn-success"> Click to view Pokemons</button>
-						</Link>
-					</div>
-				</div>
-			</div>
-		</React.Fragment>
-	);
+          <div className="col-12" id="button-Start">
+            <Link to="/pokemons">
+              <button className="btn btn-success">
+                {" "}
+                Click to view Pokemons
+              </button>
+            </Link>
+            <div className="mt-3">Para utilizar el servicio web</div>
+            <div>
+              Descargar el{" "}
+              <a href="https://chrome.google.com/webstore/detail/cors-unblock/lfhmikememgdcahcdlaciloancbhjino?hl=es-419">
+                <b>Plugin</b>
+              </a>{" "}
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
